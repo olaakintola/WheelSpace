39b1eeb2b54535e9ceaa4a9fe943c1ff626c2a0d
Ola Akintola <olanipekun_akin@yahoo.co.uk>
2021-01-09 21:01:13 +0000

    created Home Screen Activity

--- app/src/main/java/com/example/wheelspace/MainActivity.java
+++ app/src/main/java/com/example/wheelspace/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.wheelspace;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnMyStatus, btnRealtimeAccess, btnPlanMyJourney, btnForum, btnAbout;
+    private TextView txtAppName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        intiViews();
+    }
+
+    private void intiViews() {
+        btnMyStatus = findViewById(R.id.btnMyStatus);
+        btnRealtimeAccess = findViewById(R.id.btnRealtimeAccess);
+        btnPlanMyJourney = findViewById(R.id.btnPlanMyJourney);
+        btnForum = findViewById(R.id.btnForum);
+        btnAbout = findViewById(R.id.btnAbout);
     }
 }
