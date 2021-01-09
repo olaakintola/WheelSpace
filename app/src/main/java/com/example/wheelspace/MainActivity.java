bf951aad47b682f9dc88135020999ea590a367cf
Ola Akintola <olanipekun_akin@yahoo.co.uk>
2021-01-09 21:43:31 +0000

    created My Status, Realtime Access, Plan My Journey, Forum Activities

--- app/src/main/java/com/example/wheelspace/MainActivity.java
+++ app/src/main/java/com/example/wheelspace/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.wheelspace;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -17,6 +19,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         intiViews();
+
+        btnMyStatus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MyStatusActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnRealtimeAccess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RealtimeAccessActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnPlanMyJourney.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PlanMyJourneyActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnForum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ForumActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void intiViews() {
---
+++ app/src/main/java/com/example/wheelspace/PlanMyJourneyActivity.java
@@ -0,0 +1,14 @@
+package com.example.wheelspace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlanMyJourneyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plan_my_journey);
+    }
+}
