676cf4761e5b79fe469abd274683ab648b0fd6b2
Ola Akintola <olanipekun_akin@yahoo.co.uk>
2021-01-10 14:28:44 +0000

    Got the timepicker working in the My Status Activity

--- app/src/main/java/com/example/wheelspace/MyStatusActivity.java
+++ app/src/main/java/com/example/wheelspace/MyStatusActivity.java
@@ -2,13 +2,49 @@ package com.example.wheelspace;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class MyStatusActivity extends AppCompatActivity {
+
+    EditText edtTxtTimePicker;
+    TimePickerDialog timePickerDialog;
+    Calendar calendar;
+    int currentHour;
+    int currentMinute;
+    String amPm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_status);
+
+        edtTxtTimePicker = findViewById(R.id.edtTxtTimePicker);
+        edtTxtTimePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calendar = Calendar.getInstance();
+                currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+                currentMinute = calendar.get(Calendar.MINUTE);
+
+                timePickerDialog = new TimePickerDialog(MyStatusActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+                        if(hourOfDay >= 12){
+                            amPm = "PM";
+                        }else{
+                            amPm = "AM";
+                        }
+                        edtTxtTimePicker.setText(String.format("%02d:%02d", hourOfDay, minute) + amPm );
+                    }
+                }, currentHour, currentMinute, false);
+                timePickerDialog.show();
+            }
+        });
     }
 }
