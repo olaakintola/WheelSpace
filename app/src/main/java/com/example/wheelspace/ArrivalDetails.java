1585da0c66407c06aa455f0ce08261e6715a5e00
Ola Akintola <olanipekun_akin@yahoo.co.uk>
2021-01-13 22:41:48 +0000

    created model class which is used as a JSON model(POJO)

---
+++ app/src/main/java/com/example/wheelspace/ArrivalDetails.java
@@ -0,0 +1,17 @@
+package com.example.wheelspace;
+
+public class ArrivalDetails {
+    private int delay;
+
+    public ArrivalDetails(int delay) {
+        this.delay = delay;
+    }
+
+    public int getDelay() {
+        return delay;
+    }
+
+    public void setDelay(int delay) {
+        this.delay = delay;
+    }
+}
---
+++ app/src/main/java/com/example/wheelspace/DepartureDetails.java
@@ -0,0 +1,17 @@
+package com.example.wheelspace;
+
+public class DepartureDetails {
+    private int delay;
+
+    public DepartureDetails(int delay) {
+        this.delay = delay;
+    }
+
+    public int getDelay() {
+        return delay;
+    }
+
+    public void setDelay(int delay) {
+        this.delay = delay;
+    }
+}
