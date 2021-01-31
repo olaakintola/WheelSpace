37c7378c9469983168061d8be050612f95336c7c
Ola Akintola <olanipekun_akin@yahoo.co.uk>
2021-01-31 20:21:08 +0000

    added local time to the values being sent from realtime access to realtime results activity

--- app/src/main/java/com/example/wheelspace/RealtimeResultActivity.java
+++ app/src/main/java/com/example/wheelspace/RealtimeResultActivity.java
@@ -2,16 +2,28 @@ package com.example.wheelspace;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class RealtimeResultActivity extends AppCompatActivity {
+    
+    String origin;
+    String goingTo;
+    String localTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_realtime_result);
+
+        Intent intent = getIntent();
+
+        origin = intent.getStringExtra("originKey");
+        goingTo = intent.getStringExtra("goingToKey");
+        localTime = intent.getStringExtra("localTimeKey");
     }
+
 }
