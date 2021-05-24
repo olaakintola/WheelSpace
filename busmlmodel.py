4f2215fe9f9e73b3d9d0860c19c780d4478783ff
olaakintola <olanipekun_akin@yahoo.co.uk>
2021-05-24 19:50:59 +0100

    cleaned the code in busmlmodel.py

--- busmlmodel.py
+++ busmlmodel.py
@@ -4,34 +4,13 @@ from joblib import load
 import pandas as pd
 import numpy as np
 from collections import OrderedDict
-## do I need to import one hot encoding?
+
 
 app = Flask(__name__)
-
-##@app.route("/")
-##def hello():
-##    req = request.args
-##    print(req)
-##    return "Hello "
-
-@app.route('/helloworld',methods=['GET'])
-def hello():
-    req = request.args
-    req =req.to_dict()
-    print('during view')
-    return str("Hello " + req['name'])
-
-
-@app.route('/addpost',methods=['POST'])
-def add():
-    req = request.data
-    req =(json.loads(req) )
-    return str( req["a"] + req["b"] )
 
 
 @app.route('/predict',methods=['POST'])
 def predict():
-  ##  user_input = request.json
     temp_input = json.loads(request.data)
 
     print(temp_input)
@@ -60,8 +39,6 @@ def predict():
     d = json.dumps( dictList.append(user_output) )
     print(dictList)
     print(d)
-
-   ## input_df = pd.DataFrame(user_input)
     
     input_df = pd.DataFrame(dictList )
     
@@ -71,28 +48,11 @@ def predict():
     preds_input = clf.predict(input_one_hot)
     prediction = list(preds_input)
     return jsonify({'prediction': str(prediction)})
-    
-    
-##@app.route('/predict',methods=['POST'])
-##def predict():
-    ##user_input = json.loads(request.data)
-
-    ##print(user_input)
-    
-
-    ##input_df = pd.DataFrame(user_input)
-    ##input_one_hot = ohe.transform(input_df)
-    ##preds_input = clf.predict(input_one_hot)
-    ##prediction = list(preds_input)
-    ##return jsonify({'prediction': str(prediction)})
-
+   
 
 if __name__== "__main__":
 
     clf = load("busmodel.pkl")
-##    Filename = 'busmodel.pkl'
-##    with open(Filename, 'rb') as file:
-##        clf = pickle.load(file)
     print("Model loaded")
     ohe = load("onehot_data")
     print("One Hot Encoding loaded")
